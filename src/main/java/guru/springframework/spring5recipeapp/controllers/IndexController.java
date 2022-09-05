bff55fcf64e4cd31dd0759b68cf4f55ef49ae3b0
Akshar <akshar.sivnarain@gmail.com>
2022-09-05 09:48:48 +0200

    Added in Recipe and Notes entities and relationships.

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -9,6 +9,7 @@ public class IndexController
     @RequestMapping({"","/","/index"})
     public String getIndexPage()
     {
+        System.out.println("testing ........");
         return "index";
     }//
 }
